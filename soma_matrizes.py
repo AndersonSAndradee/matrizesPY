8fa0bd3ce04d9ef589098111aaf1bf064ffecff4
Anderson Silva de Andrade <144751848+AndersonSAndradee@users.noreply.github.com>
2023-09-21 17:38:10 -0300

    Update soma_matrizes.py

--- soma_matrizes.py
+++ soma_matrizes.py
@@ -1,10 +1,10 @@
 '''Função = soma matrizes, "recebe como parâmetro matriz1 e matriz2, se as matrizes possuem mesmas proporções retorna o valor da soma delas, caso contrário retorna o valor False" '''
 def soma_matrizes(m1, m2): 
     if len(m1) == len(m2) and len(m1[0]) == len(m2[0]):  # Compara se é válido a soma das matrizes
-        matriz_final = []  # Inicializa a matriz final vazia
+        matriz_final = []  # matriz principal(linhas)
 
         for i in range(len(m1)):
-            linha_resultado = []  # Inicializa uma lista vazia para a matriz final
+            linha_resultado = []  # matrizes secundárias(colunas)
             for j in range(len(m1[0])):
                 
                 elemento1 = m1[i][j]
